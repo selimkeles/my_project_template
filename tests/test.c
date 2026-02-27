153811008e2e126c3af36287527c137e210720e3
selimkeles <mehmetselimkeles@outlook.com>
2026-02-28 00:52:59 +0300

    test fix

--- tests/test.c
+++ tests/test.c
@@ -1,13 +1,8 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include "unity.h"
 
-int main() {
-    
-    // Test Function Capabilities calling the modules one by one
-
-    printf("\nEverything OK\n");
+int main(void)
+{
+    printf("All tests passed\n");
     return 0;
 }
 
